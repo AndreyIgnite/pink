ff942068f88b5962b53756045f99335495bbaa6c
Andrey <mozobretenie@gmail.com>
2024-04-09 20:51:48 +0300

    первый слайдер в мобильной версии

--- source/less/blocks/slider-review.js
+++ source/less/blocks/slider-review.js
@@ -1,8 +1,9 @@
 let sliderList = document.querySelector('.slider-review__list');
 let sliderReviews = document.querySelectorAll('.reviews__item');
-let sliderDots = document.querySelectorAll('.slider-review__toggle');
+let sliderDots = document.querySelectorAll('.slider-review__dot');
 let sliderBtnNext = document.querySelector('.reviews__button--next')
 let sliderBtnPrev = document.querySelector('.reviews__button--prev')
+
 
 let sliderCount = 0;
 let sliderWidth;
@@ -10,7 +11,17 @@ let sliderWidth;
 window.addEventListener('resize', showSlider);
 sliderBtnNext.addEventListener('click', nextSlide);
 sliderBtnPrev.addEventListener('click', prevSlide);
+sliderDots.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if(!(item.classList.contains('slider-review__dot--current'))) {
+      sliderCount = index;
+      rollSlider();
+      document.querySelector('.slider-review__dot--current').classList.remove('slider-review__dot--current')
+      sliderDots[index].classList.add('slider-review__dot--current')
+    }
 
+  })
+})
 
 function showSlider() {
   sliderWidth = document.querySelector('.slider-review').offsetWidth;
@@ -21,12 +32,13 @@ function showSlider() {
 showSlider()
 
 function nextSlide() {
+
   sliderCount++;
   if (sliderCount >= sliderReviews.length) {
     sliderCount = 0;
   }
+  switchDot()
   rollSlider()
-  console.log(sliderCount)
 }
 
 function prevSlide() {
@@ -34,10 +46,15 @@ function prevSlide() {
   if (sliderCount < 0) {
     sliderCount = sliderReviews.length - 1;
   }
+  switchDot()
   rollSlider()
-  console.log(sliderCount)
 }
 
 function rollSlider() {
   sliderList.style.transform = `translateX(${-sliderCount * sliderWidth}px)`
+}
+
+function switchDot() {
+  document.querySelector('.slider-review__dot--current').classList.remove('slider-review__dot--current')
+  sliderDots[sliderCount].classList.add('slider-review__dot--current')
 }
