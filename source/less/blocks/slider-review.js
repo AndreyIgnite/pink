dd5d839466f071089d3e1c9404cfc81a0068fca8
Andrey <mozobretenie@gmail.com>
2024-04-15 21:02:42 +0300

    dragndrop swiper

--- source/less/blocks/slider-review.js
+++ source/less/blocks/slider-review.js
@@ -1,123 +1,87 @@
-let sliderList = document.querySelector(".slider-review__list");
-let sliderReviews = document.querySelectorAll(".slider-review__item");
-let sliderDots = document.querySelectorAll(".toggles__dot");
-let sliderBtnNext = document.querySelector(".reviews__button--next");
-let sliderBtnPrev = document.querySelector(".reviews__button--prev");
+const TRIGGER_OFFSET = 7; //порог срабатывания свайпа, 1/7 от ширины слайда
+let sliderList = document.querySelector(".slider-review__list"), //обертка слайдера
+sliderReviews = document.querySelectorAll(".slider-review__item"), //массив со слайдами
+sliderDots = document.querySelectorAll(".toggles__dot"), //массив с пагинациями
+sliderBtnNext = document.querySelector(".reviews__button--next"), //вперед, только для десктопа
+sliderBtnPrev = document.querySelector(".reviews__button--prev"), //назад
+touchSurface = document.querySelector(".slider-review"), //зона свайпа
+sliderCount = 0, //счетчик слайдов, индикатор текущего слайда
+sliderWidth, //ширина видимой части слайдера, ширина одного слайда
+startX, //начальная точка касания, для тачпадов
+dragndropSlidePosition, //позиция ленты слайдеров после оттаскивания слайда при свайпе на произвольное расстояние в px для translateX
+dragndropSlideShit, //величина сдвига
+currentSlidePosition; // текущая позиция ленты слайдера, (величина кратна sliderCount)
 
-let sliderCount = 0;
-let sliderWidth;
+showSlider(sliderCount); //пересчет ширины слайда при загрузке страницы
 
-window.addEventListener("resize", showSlider);
-sliderBtnNext.addEventListener("click", nextSlide);
-sliderBtnPrev.addEventListener("click", prevSlide);
-sliderDots.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    sliderCount = index;
-    rollSlider(sliderCount);
-  });
-});
-
-function showSlider() {
-  sliderWidth = document.querySelector(".slider-review").offsetWidth;
-  sliderList.style.width = sliderWidth * sliderReviews.length + "px";
-  sliderReviews.forEach((item) => (item.style.width = sliderWidth + "px"));
-  rollSlider(sliderCount);
+function showSlider(index) { //функция пересчета ширины слайдера под экран
+  sliderWidth = document.querySelector(".slider-review").offsetWidth; //определяем ширину контейнера слайдера
+  sliderList.style.width = sliderWidth * sliderReviews.length + "px"; //даём ширину обёртке слайдера, умноженную на кол-во слайдов
+  sliderReviews.forEach((item) => (item.style.width = sliderWidth + "px")); //даём каждому слайду ширину видимой области, чтобы слайды ровно уместились в обертку
+  rollSlider(index); //двигаем ленту чтобы слайд с номеров index оказался напротив ровно видимой области
 }
-showSlider();
-
-function nextSlide() {
+function nextSlide(index) {
   sliderCount++;
   if (sliderCount >= sliderReviews.length) {
     sliderCount = 0;
   }
-  rollSlider(sliderCount);
+  rollSlider(index);
 }
-
-function prevSlide() {
+function prevSlide(index) {
   sliderCount--;
   if (sliderCount < 0) {
     sliderCount = sliderReviews.length - 1;
   }
-  rollSlider(sliderCount);
+  rollSlider(index);
 }
-
-function rollSlider(index) {
-  switchDot(sliderCount);
-  sliderList.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+function rollSlider(index) { //ролл ленты(обёртки) слайдера в опредёлнную позицию
+  sliderList.style.transform = `translateX(${-sliderCount * sliderWidth}px)`; //в зависимости от индикатора едем на опреденный слайд
+  switchDot(index); //переключаем пагинацию
+  currentSlidePosition = sliderWidth * -sliderCount; //
 }
-
-function switchDot(index) {
-  sliderDots.forEach((item) => item.classList.remove("toggles__dot--current"));
-  sliderDots[index].classList.add("toggles__dot--current");
+function switchDot(index) { //переключатель пагинации
+  sliderDots.forEach((item) => item.classList.remove("toggles__dot--current")); //убираем текущую пагинацию
+  sliderDots[index].classList.add("toggles__dot--current"); //ебашим на нужную
 }
-
-let touchSurface = document.querySelector(".slider-review"),
-  distX,
-  dustY,
-  startX,
-  startY,
-  dragndropSlidePosition,
-  dragndropSlideShit,
-  currentSlidePosition;
-
-touchSurface.addEventListener(
-  "touchstart",
-  function (e) {
-    sliderList.style.transition = "all 0s";
-    let event = e.changedTouches[0];
-    startX = event.pageX;
-    startY = event.pageY;
-    dragndropSlideShit = 0;
-    currentSlidePosition = sliderWidth * -sliderCount;
-    e.preventDefault();
-  },
-  false
-);
-
-touchSurface.addEventListener(
-  "touchmove",
-  function (e) {
-    let event = e.changedTouches[0];
-    dragndropSlideShit = event.pageX - startX;
-    dragndropSlidePosition = currentSlidePosition + dragndropSlideShit;
-    if (event.pageY - startY <= 100) {
+window.addEventListener('load', function () {
+  console.log('done')
+  window.addEventListener("resize", function () { //пересчитываем ширину для меняется ширина экрана
+    showSlider(sliderCount);
+  });
+  sliderBtnNext.addEventListener("click", function () {
+    nextSlide(sliderCount);
+  });
+  sliderBtnPrev.addEventListener("click", function () {
+    prevSlide(sliderCount);
+  });
+  sliderDots.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      sliderCount = index;
+      rollSlider(sliderCount);
+    });
+  });
+  touchSurface.addEventListener("touchstart", function (e) {
+      sliderList.style.transition = "all 0s";
+      let event = e.changedTouches[0];
+      startX = event.pageX;
+      dragndropSlideShit = 0;
+      e.preventDefault();
+    }, false);
+  touchSurface.addEventListener("touchmove", function (e) {
+      let event = e.changedTouches[0];
+      dragndropSlideShit = event.pageX - startX;
+      dragndropSlidePosition = currentSlidePosition + dragndropSlideShit;
       sliderList.style.transform = `translateX(${dragndropSlidePosition}px)`;
-    }
-    e.preventDefault();
-  },
-  false
-);
-
-touchSurface.addEventListener(
-  "touchend",
-  function (e) {
-    sliderList.style.transition = "all 0.4s";
-    if (Math.abs(dragndropSlideShit) < sliderWidth / 7.1) {
-      rollSlider(sliderCount);
-    }
-    if (
-      dragndropSlideShit < -sliderWidth / 7 &&
-      !(sliderCount == sliderReviews.length - 1)
-    ) {
-      sliderCount++;
-      if (sliderCount >= sliderReviews.length) {
-        sliderCount = 0;
+      e.preventDefault();
+    }, false);
+  touchSurface.addEventListener("touchend", function (e) {
+      sliderList.style.transition = "all 0.4s";
+      if (dragndropSlideShit < -sliderWidth / TRIGGER_OFFSET && !(sliderCount == sliderReviews.length - 1)) {
+        sliderCount++;
+      }
+      if (dragndropSlideShit > sliderWidth / TRIGGER_OFFSET && !(sliderCount == 0)) {
+        sliderCount--;
       }
       rollSlider(sliderCount);
-    } else {
-      rollSlider(sliderCount);
-    }
-    if (dragndropSlideShit > sliderWidth / 7 && !(sliderCount == 0)) {
-      sliderCount--;
-      if (sliderCount < 0) {
-        sliderCount = sliderReviews.length - 1;
-      }
-      rollSlider(sliderCount);
-    } else {
-      rollSlider(sliderCount);
-    }
-  },
-  false
-);
-
-function swipe(swipe) {}
+    }, false);
+})
