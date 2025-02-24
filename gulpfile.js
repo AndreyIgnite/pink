f3d6e1208b094363efb996e9483f7391dbc95bdb
Андрей Капелькин <mozobretenie@gmail.com>
2025-02-24 16:13:32 +0300

    сборка

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ import rename from 'gulp-rename';
 import terser from 'gulp-terser';
 import svgstore from 'gulp-svgstore';
 import webp from 'gulp-webp';
+import {deleteAsync} from 'del';
 //import squoosh from 'gulp-libsquoosh';
 
 //Images
@@ -20,14 +21,19 @@ export const image = () => {
   .pipe(gulp.dest('build/img'));
 }
 
+
+export const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png,svg}')
+  .pipe(gulp.dest('build/img'));
+}
+
 // Webp
 
 export const createWebp = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
+  return gulp.src("source/img/**/*.{jpg,png}")
   .pipe(webp({quality: 90}))
   .pipe(gulp.dest('build/img'))
 }
-
 
 
 /* HTML */
@@ -55,11 +61,32 @@ export const styles = () => {
 }
 
 
+//Copy
+
+export const copy = (done) => {
+  gulp.src(["source/fonts/*.{woff, woff2}",
+    "source/*.{ico,png, svg}",
+    "source/site.webmanifest",
+    "source/img/**/*.{png,jpg,svg}"
+  ], {base: "source"
+
+  })
+  .pipe(gulp.dest("build"))
+  done();
+}
+
+//Clean
+
+export const clean = () => {
+  return deleteAsync("build");
+}
+
+
 
 
 //Scripts
 
-export const script = () => {
+export const scripts = () => {
   return gulp.src("source/**/*.js")
   .pipe(terser())
   .pipe(gulp.dest('build/'))
@@ -83,18 +110,35 @@ const server = (done) => {
   done();
 }
 
+//Build
+
+
+export const build = gulp.series(
+  clean,
+  copy,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    createWebp
+  ),
+);
+
+
 
 
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
+  gulp.watch('soucre/less/**/*.js', gulp.series(scripts));
+  gulp.watch('soucre/js/*.js', gulp.series(scripts));
   gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 
 export default gulp.series(
-  script, styles, html, server, watcher
+  server, watcher
 );
 
 
